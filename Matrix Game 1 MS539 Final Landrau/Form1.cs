e3fc0422c32ca4a099607ecca4f7489a172a0d37
Jay7514 <109188190+Jay7514@users.noreply.github.com>
2026-06-29 07:57:38 +0200

    For Review
    Review

---
+++ Matrix Game 1 MS539 Final Landrau/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Matrix_Game_1_MS539_Final_Landrau
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void OnKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Core.KeyUp)
+                Core.IsUp = true;
+            if (e.KeyCode == Core.KeyDown)
+                Core.IsDown = true;
+            if (e.KeyCode == Core.KeyRight)
+                Core.IsUp = true;
+            if (e.KeyCode == Core.KeyLeft)
+                Core.IsLeft = true;
+
+        }
+
+        private void OnKeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Core.KeyUp)
+                Core.IsUp = false;
+            if (e.KeyCode == Core.KeyDown)
+                Core.IsDown = false;
+            if (e.KeyCode == Core.KeyRight)
+                Core.IsUp = false;
+            if (e.KeyCode == Core.KeyLeft)
+                Core.IsLeft = false;
+
+
+        }
+
+
+
+                private void OnKeyUp(object sender, KeyEventArgs e)
+        {
+
+        }
+    }
+}
+
+
---
+++ Matrix Game 1 MS539 Final Landrau/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Matrix_Game_1_MS539_Final_Landrau
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
